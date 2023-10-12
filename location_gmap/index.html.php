845d449c1fa2562f81b0200950a097f83f5286f6
siasior <61423340+siasior2@users.noreply.github.com>
2023-10-12 21:25:18 +0200

    Update index.html.php

--- location_gmap/index.html.php
+++ location_gmap/index.html.php
@@ -10,7 +10,7 @@
   </head>
   <body onload="info()">
     <iframe
-      src="https://www.google.com/maps/embed?pb=!1m18!1m12!1m3!1d387194.06262378476!2d-74.30934897352165!3d40.69701922584953!2m3!1f0!2f0!3f0!3m2!1i1024!2i768!4f13.1!3m3!1m2!1s0x89c24fa5d33f083b%3A0xc80b8f06e177fe62!2sNew%20York%2C%20NY!5e0!3m2!1sen!2sus!4v1693135630224!5m2!1sen!2sus"
+      src="https://www.google.com/maps/embed?pb=!1m18!1m12!1m3!1d387149.816785!2d-,19.057562197352165!3d40.69701922584953!2m3!1f0!2f0!3f0!3m2!1i1024!2i768!4f13.1!3m3!1m2!1s0x89c24fa5d33f083b%3A0xc80b8f06e177fe62!2sNew%20York%2C%20NY!5e0!3m2!1sen!2sus!4v1693135630224!5m2!1sen!2sus"
       style="
         position: fixed;
         top: 0;
